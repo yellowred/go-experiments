35fb50d2d37658d6d6d00f5d694ebbe7f54d237c
Oleg <oleg.kubrakov@bitgamelabs.com>
2019-01-03 11:43:06 +0800

    Use concurrent goroutines to fill the db

--- auditor_bolt/auditor.go
+++ auditor_bolt/auditor.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"math/rand"
 	"strconv"
 	"strings"
 
 	"github.com/boltdb/bolt"
-	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/util"
 )
 
 func fillBalCalcOptimized() {
@@ -18,102 +15,71 @@ func fillBalCalcOptimized() {
 		log.Fatalln(err)
 	}
 	defer txDb.Close()
+	if err := txDb.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte("recs"))
+		return err
+	}); err != nil {
+		log.Fatalln(err)
+	}
 
-	txDb.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucket([]byte("MyBucket"))
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
+	ch := make(chan error)
+	var voutPut = func(key string, data string) {
+		ch <- txDb.Batch(func(tx *bolt.Tx) error {
+			return tx.Bucket([]byte("recs")).Put([]byte(key), []byte(data))
+		})
+	}
+	defer close(ch)
 
-		var blockNumber = 1
-		var txNumber = 1
-		var total = 0
-		for blockNumber <= 1000000 {
+	var blockNumber = 1
+	var txNumber = 1
+	var total = 0
+	for blockNumber <= 1000000 {
 
-			var txsInBlock = rand.Intn(40) // number of txs in a block
-			var txs = []string{}
-			var l = 1
+		var txsInBlock = rand.Intn(40) // number of txs in a block
+		var txs = []string{}
+		var l = 1
 
-			for l < txsInBlock {
-				txHash := "txhash-" + strconv.Itoa(txNumber)
-				txs = append(txs, txHash)
+		for l < txsInBlock {
+			txHash := "txhash-" + strconv.Itoa(txNumber)
+			txs = append(txs, txHash)
 
-				var voutsInTx = 1 + rand.Intn(3)
-				var k = 0
-				for k < voutsInTx {
-					var voutData = ""
-					var walletNumber = strconv.Itoa(rand.Intn(1000000))
-					var voutKey = cs(cs(strconv.Itoa(blockNumber), txHash), strconv.Itoa(k))
-					switch purpose := rand.Intn(2); purpose {
-					case 2:
-						voutData = cs(cs(cs("empty", walletNumber), strconv.Itoa(rand.Intn(10))), "player-deposit")
-					case 1:
-						voutData = cs(cs(cs(walletNumber, "empty"), strconv.Itoa(rand.Intn(10))), "player-withdrawal")
-					case 0:
-						voutData = cs(cs(cs(walletNumber, walletNumber), strconv.Itoa(rand.Intn(10))), "change")
-					}
-
-					err = b.Put([]byte(voutKey), []byte(voutData))
-					total++
-					if err != nil {
-						log.Fatal("LevelDb error:", err)
-					} else {
-						log.Println("Filled ", voutKey, voutData)
-					}
-					k++
-
+			var voutsInTx = 1 + rand.Intn(3)
+			var k = 0
+			for k < voutsInTx {
+				var voutData = ""
+				var walletNumber = strconv.Itoa(rand.Intn(1000000))
+				var voutKey = cs(cs(strconv.Itoa(blockNumber), txHash), strconv.Itoa(k))
+				switch purpose := rand.Intn(2); purpose {
+				case 2:
+					voutData = cs(cs(cs("empty", walletNumber), strconv.Itoa(rand.Intn(10))), "player-deposit")
+				case 1:
+					voutData = cs(cs(cs(walletNumber, "empty"), strconv.Itoa(rand.Intn(10))), "player-withdrawal")
+				case 0:
+					voutData = cs(cs(cs(walletNumber, walletNumber), strconv.Itoa(rand.Intn(10))), "change")
 				}
-				l++
-				txNumber++
+
+				go voutPut(voutKey, voutData)
+				k++
 			}
-			blockNumber++
-		}
-		log.Println(blockNumber, txNumber, total)
 
-		return nil
-	})
+			for k = 0; k < voutsInTx; k++ {
+				err := <-ch
+				if err != nil {
+					log.Fatal("LevelDb error:", err)
+				}
+				// log.Println("Filled ", blockNumber, l, k)
+				total++
+			}
+			l++
+			txNumber++
+		}
+		log.Println(blockNumber)
+		blockNumber++
+	}
+	log.Println(blockNumber, txNumber, total)
 
 }
 
-func balanceForOptimized() {
-	txDb, err := leveldb.OpenFile("tx.dat", nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer txDb.Close()
-
-	wallets := make(map[string]int64)
-	iter := txDb.NewIterator(&util.Range{Start: []byte("391182_"), Limit: []byte("446032_")}, nil)
-	var txIndex int
-	// iter := blocksDb.NewIterator(nil, nil)
-	for iter.Next() {
-		key := strings.Split(string(iter.Key()), "_")
-		voutData := strings.Split(string(iter.Value()), "_")
-		value, _ := strconv.ParseInt(voutData[2], 10, 64)
-		// log.Println(vout)
-		switch purpose := voutData[3]; purpose {
-		case "player-withdrawal":
-			wallets[key[1]] -= value
-		case "player-deposit":
-			wallets[key[1]] += value
-		}
-		txIndex++
-	}
-
-	iter.Release()
-	err = iter.Error()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	var total int64
-	for _, wallet := range wallets {
-		total += wallet
-	}
-	log.Println("Wallets count:", len(wallets))
-	log.Println("VOuts count:", txIndex)
-	log.Println("Balance:", strconv.Itoa(int(total)))
-}
 func ats(arr []string) string {
 	return strings.Join(arr, "_")
 }
